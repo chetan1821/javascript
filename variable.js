4c9c094b00a252e88caacdaed3eb4393cd305d6a
chetan1821 <cp7292367@gmail.com>
2024-07-22 16:43:51 +0530

    variables in Js

---
+++ variable.js
@@ -0,0 +1,20 @@
+// variable in js
+const accountId=644;
+let accountEmail="chetan123@gmail.com";
+var accountPassword="123456";
+accountCity="surat";
+let accountstate;
+// accountId=1;  not allowed because account id is const..
+
+console.log(accountId);
+// console.log(accountEmail);
+// console.log(accountPassword);
+// console.log(accountCity);
+/**
+    prefer not to use var
+    because of issue in block scope and function scope
+
+ */
+console.table([accountId,accountEmail,accountPassword,accountCity,accountstate]);
+
+
