29a11d2117c5c74646ffdc232c6884ee7bfbf4b6
chetan1821 <cp7292367@gmail.com>
2024-07-22 17:52:44 +0530

    define datatypes and datatypes Conversion

---
+++ datatype.js
@@ -0,0 +1,28 @@
+// Data types in js
+let fname ="chetan";
+console.log(fname);
+let roll_no = 644;
+console.log(roll_no);
+let address;
+let logIn =true; //this data type is boolean
+
+// primitive data types
+/**
+    number => 2 power 53
+    bigint
+    string => " "
+    boolean =>true/false
+    null => empty value or standalone value
+    undefined => its not define value only define the variable
+    symbol =>unique
+
+    
+ */
+// non primitive data types
+// object
+
+console.log(typeof(fname));//string
+console.log(typeof(roll_no));//number
+console.log(typeof(address));//undefined 
+console.log(typeof(null));//object
+console.log(typeof(logIn));//Boolean
---
+++ datatype_conversion.js
@@ -0,0 +1,20 @@
+// Data type converion is convert the data type other data type
+
+   // this conversion is Number to string
+let score =45;
+console.log(typeof score);
+let score_str =String (score);
+console.log(typeof score_str);
+
+//this conversion is string to number 
+let fnames ="2323";//we can decalre the "chetan2323" is not convert the number
+console.log(typeof fnames);
+let fnumber = Number (fnames);
+console.log(fnumber);
+
+//Boolean =>true or false or 1=>true and 0 => false
+let temp = 1;
+console.log(typeof temp);
+let temp1 = Boolean (temp)
+console.log(temp1);
+
