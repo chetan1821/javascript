227463e341a86f13eb630a710bd03d24243916ef
chetan1821 <cp7292367@gmail.com>
2024-07-24 20:13:51 +0530

    Date and time structure

---
+++ date_Time.js
@@ -0,0 +1,22 @@
+// Dates
+let myDate = new Date();
+// console.log(myDate);
+// console.log(myDate.getTime()); //its return the mili second
+// console.log(myDate.toISOString());//its return the ios formmat
+
+// let myCreatedDate = new Date("01-14-2023");
+// console.log(myCreatedDate.toLocaleString()); //14/1/2023, 12:00:00 am
+// console.log(myCreatedDate.toLocaleDateString()); // 14/1/2023
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp);
+console.log(Math.floor (Date.now()/1000));
+
+// similer method 
+/**
+ getMonth
+ getYear
+
+ */
+let newdate = new Date()
+console.log(newdate.getDay());
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+let myDate = new Date();
+// console.log(myDate);
+// console.log(myDate.getTime()); //its return the mili second
+// console.log(myDate.toISOString());//its return the ios formmat
