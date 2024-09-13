ce8d017a72d7a34413c21b1523c843a372d3cb70
chetan1821 <cp7292367@gmail.com>
2024-09-13 23:28:06 +0530

    Scope of js

--- scope.js
+++ scope.js
@@ -1,4 +1,3 @@
-
 function one(){
     const userName = "chetan"
     function two(){
@@ -19,13 +18,17 @@ if (true) {
         console.log(username + terminal1);
     }
 }
-
+// Function Declaration vs. Function Expression:
+//A function declaration is when you define a function using the function keyword followed by the name of the function.
 function oneFun(){
     console.log("this Function one" );
 }
-
 oneFun()
-twoFun()
+
+
+//A function expression is when you define a function and assign it to a variable.
+
 const twoFun = function() {
     console.log("this Function two" );
 }
+twoFun()
