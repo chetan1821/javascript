c29db71cf69bbcbc6a1f5e5f269317fca985b016
chetan1821 <cp7292367@gmail.com>
2024-09-14 11:52:30 +0530

    Immediately Invoked Function Expression

---
+++ IIfeFun.js
@@ -0,0 +1,11 @@
+//Immediately Invoked Function Expression (IIFE)
+
+(function chai(){
+    //named IIFE
+    console.log('DB Connected');
+    
+})();
+((name)=>{
+    console.log(`DB Connected Two ${name}`);
+    
+})('chetan')
